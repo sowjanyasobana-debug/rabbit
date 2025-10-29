97bff66718ffbee7b2c12bff08b0abe23b3ef83d
sowjanyasobana-debug <sowjanyasobana@gmail.com>
2025-10-29 11:56:18 +0530

    Add files via upload

---
+++ cal.js
@@ -0,0 +1,16 @@
+function appendValue(value){
+    document.getElementById("result").value +=value;
+}
+function clearResult(){
+    document.getElementById("result").value ="";
+}
+function calculate(){
+    let expression=document.getElementById("result").value;
+try{
+        document.getElementById("result").value =eval(expression);
+    }
+    catch(error){
+document.getElementById("result").value ="Error";
+    }
+    
+}
